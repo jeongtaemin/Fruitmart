c4aa603b01a929158b074000797d6140c35e2d2d
jt01052552295 <jt01052552295@gmail.com>
2021-11-24 21:20:34 +0900

    Initial Commit

---
+++ Fruitmart/FruitmartApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitmartApp.swift
+//  Fruitmart
+//
+//  Created by admin on 2021/11/24.
+//
+
+import SwiftUI
+
+@main
+struct FruitmartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
